082bfe2ef1f5e7c0e919aeec9ec37fa50fa9a54d
Carl Jason <73461353+Carl-bot-Jason@users.noreply.github.com>
2023-08-02 17:35:02 +0530

    Added audio, audio toggle, finished

--- src/app.js
+++ src/app.js
@@ -1,22 +1,48 @@
-let bgCanvas = $('#bg-canvas'), fgCanvas = $('#fg-canvas'), gradDiv = $('#gradient');
+let bgCanvas = $('#bg-canvas'),
+	fgCanvas = $('#fg-canvas'),
+	gradDiv = $('#gradient'),
+	body = $('body');
 
-bgCanvas.get(0).width = $('body').innerWidth();
-bgCanvas.get(0).height = $('body').innerHeight();
+bgCanvas.get(0).width = body.innerWidth();
+bgCanvas.get(0).height = body.innerHeight();
+fgCanvas.get(0).width = body.innerWidth();
+fgCanvas.get(0).height = body.innerHeight();
+gradDiv.height(body.innerHeight());
 
-fgCanvas.get(0).width = $('body').innerWidth();
-fgCanvas.get(0).height = $('body').innerHeight();
-
-gradDiv.height($('body').innerHeight());
+let mute = $('#audio-off'), nomute = $('#audio-on'), mutebox = $('#mute-box');
+nomute.css('display', 'none');
 
 let canW = bgCanvas.width(),
 	canH = bgCanvas.height(),
-	baseHeight = canH * 0.7,
+	baseHeight = canH * 0.85,
 	arcCount = 13,
-	radIncrement = ((canW * 0.6 * 0.5) - 80) / (arcCount - 1),
-	firstOffset = 80,
+	firstOffset = body.innerWidth() / 20,
+	dotRadius = body.innerWidth() / 140,
+	radIncrement = ((canW * 0.55 * 0.5) - firstOffset) / (arcCount - 1),
 	arcConfig = [],
 	maxAngle = Math.PI * 2,
-	fillGradVelocity = 255 / 1000;
+	fillGradVelocity = 255 / 1000,
+	soundEnabled = false;
+
+function muteHandler(){
+	if(soundEnabled){
+		mute.css('display', 'initial');
+		nomute.css('display', 'none');
+	}
+	else{
+		mute.css('display', 'none');
+		nomute.css('display', 'initial');
+	}
+	soundEnabled = !soundEnabled;
+}
+
+mutebox.on('click', muteHandler);
+
+document.onvisibilitychange = () => {
+	mute.css('display', 'initial');
+	nomute.css('display', 'none');
+	soundEnabled = false;
+}
 
 let colors = [
 	'#807865',
@@ -35,12 +61,14 @@ let colors = [
 ];
 
 for(let i = 0; i < arcCount; i++){
+	let audio = new Audio(`/assets/audio/a${i+1}.mp3`);
+	audio.volume = 0.02;
 	let obj = {
 		color: colors[i],
-		angVelocity: Math.PI * (80 - i) / (15 * 60 * 1000),	
+		angVelocity: Math.PI * (80 - i) / (15 * 60 * 1000),
 		radius: firstOffset + i * radIncrement,
 		fill: 0,
-		
+		sound: audio
 	};
 	arcConfig.push(obj);
 }
@@ -54,8 +82,8 @@ let ctx = bgCanvas.get(0).getContext('2d');
 ctx.lineWidth = 4;
 ctx.strokeStyle = 'white';
 ctx.beginPath();
-ctx.moveTo(canW * 0.19, baseHeight + 2);
-ctx.lineTo(canW * 0.81, baseHeight + 2);
+ctx.moveTo(canW * 0.215, baseHeight + 2);
+ctx.lineTo(canW * 0.785, baseHeight + 2);
 ctx.stroke();
 
 // Arcs
@@ -97,17 +125,21 @@ function draw(){
 			if(!obj.fillChange){
 				obj.fill = 255;
 				obj.fillChange = true;
+				if(soundEnabled)
+					obj.sound.play();
 			}
 		}
 		else{
 			if(obj.fillChange){
 				obj.fill = 255;
 				obj.fillChange = false;
+				if(soundEnabled)
+					obj.sound.play();
 			}
 		}
 
 		ctx.beginPath();
-		ctx.arc(canW * 0.5 - obj.radius * Math.cos(angle), baseHeight - obj.radius * Math.sin(angle), 11, 0, Math.PI * 2);
+		ctx.arc(canW * 0.5 - obj.radius * Math.cos(angle), baseHeight - obj.radius * Math.sin(angle), dotRadius, 0, Math.PI * 2);
 		ctx.stroke();
 		ctx.fill();
 	}
